7dadba957a81b614af0220d4f71f7e482bd6db9d
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-19 23:06:33 +0300

    Update AdaptiveBox.kt

--- app/src/main/java/com/vasilisina/azbuka/ui/common/AdaptiveBox.kt
+++ app/src/main/java/com/vasilisina/azbuka/ui/common/AdaptiveBox.kt
@@ -3,31 +3,17 @@
 package com.vasilisina.azbuka.ui.common
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 
 @Composable
 fun AdaptiveBox(content: @Composable () -> Unit) {
-    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-        val designWidth = 1920f
-        val designHeight = 1080f
-
-        val finalScale = minOf(maxWidth.value / designWidth, maxHeight.value / designHeight)
-
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .graphicsLayer {
-                    scaleX = finalScale
-                    scaleY = finalScale
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            content()
-        }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
     }
 }

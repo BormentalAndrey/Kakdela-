7c5f97989166f85de88371a95ebb207d3a9cd441
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-19 22:14:45 +0300

    Add AdaptiveBox composable for responsive layout

---
+++ app/src/main/java/com/vasilisina/azbuka/ui/common/AdaptiveBox.kt
@@ -0,0 +1,38 @@
+// Сохранить в app/src/main/java/com/vasilisina/azbuka/ui/common/AdaptiveBox.kt
+
+package com.vasilisina.azbuka.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AdaptiveBox(content: @Composable () -> Unit) {
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        // Базовый дизайн рассчитан на 1920x1080 (ландшафт)
+        val designWidth = 1920f
+        val designHeight = 1080f
+        
+        val scaleX = maxWidth.value / designWidth
+        val scaleY = maxHeight.value / designHeight
+        val scale = minOf(scaleX, scaleY)
+        
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            content()
+        }
+    }
+}
